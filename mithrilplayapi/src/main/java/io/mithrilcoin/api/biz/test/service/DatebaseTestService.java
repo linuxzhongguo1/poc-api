77d16b9b40e07ee289ebbe115fd0cfac9e87c8ce
Kei <Kei@DESKTOP-HJ4IFLC>
2018-01-17 14:24:56 +0900

    cache annotaion apply. redis cache bulk api apply

--- mithrilplayapi/src/main/java/io/mithrilcoin/api/biz/test/service/DatebaseTestService.java
+++ mithrilplayapi/src/main/java/io/mithrilcoin/api/biz/test/service/DatebaseTestService.java
@@ -1,9 +1,11 @@
 package io.mithrilcoin.api.biz.test.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import io.mithrilcoin.api.biz.test.mapper.DatabaseTestMapper;
+import io.mithrilcoin.api.common.redis.RedisDataRepository;
 
 @Service
 public class DatebaseTestService {
@@ -11,8 +13,13 @@ public class DatebaseTestService {
 	@Autowired
 	private DatabaseTestMapper dbTestMapper;
 	
+	@Autowired
+	private RedisDataRepository<String, String> redisRepo;
+	
+	@Cacheable("dbtime")
 	public String getDbTime()
 	{
+		redisRepo.setData("dbTime",  dbTestMapper.getNow());
 		return dbTestMapper.getNow();
 	}
 	
